fe8c4ec58fa26d7762d9d1a4e2b60a235c8ee01c
srknair84 <rameshkrishnan.s@gmail.com>
2017-05-09 00:32:12 +0530

    Add files via upload

---
+++ Student_Details.java
@@ -0,0 +1,46 @@
+import java.lang.*;
+import java.io.*;
+import java.util.Scanner;
+
+class student
+{
+	String name;
+	int roll_no;
+	int sub1,sub2;
+	int total;
+	float per;
+	public static Scanner sc;
+	void getdata() 
+	{
+		
+		System.out.println ("Enter Name of Student");
+		name = sc.next();
+		System.out.println ("Enter Roll No. of Student");
+		roll_no = Integer.parseInt(sc.next());
+		System.out.println ("Enter marks out of 100 of 1st subject");
+		sub1 = Integer.parseInt(sc.next());
+		System.out.println ("Enter marks out of 100 of 2nd subject");
+		sub2 = Integer.parseInt(sc.next());
+	}
+	void show()
+	{
+		total=sub1+sub2;
+		per=(total*100)/200;
+		System.out.println ("Roll No. = "+roll_no);
+		System.out.println ("Name = "+name);
+		System.out.println ("Marks of 1st Subject = "+sub1);
+		System.out.println ("Marks of 2nd Subject = "+sub2);
+		System.out.println ("Total Marks = "+total);
+		System.out.println ("Percentage = "+per+"%");
+	}
+}
+class q2Student
+{
+	
+	public static void main(String args[]) throws IOException
+	{
+		student s=new student();
+		s.getdata();
+		s.show();
+	}
+}
